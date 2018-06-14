67c4d09236c285e9d4ad5a8cc4cd4069cb853277
External.Pallavi.Har <External.Pallavi.Har@w10duptn200s.usa.win.int.kn>
2018-06-14 03:19:19 -0400

    First Local Commit

---
+++ FirstDemo/src/test/java/KN/Tutorial/Package/FirstDemo/AppTest.java
@@ -0,0 +1,20 @@
+package KN.Tutorial.Package.FirstDemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
